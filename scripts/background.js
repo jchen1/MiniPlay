a205a1e08b5c498eba5e2fb9c1002c0f4b170a7c
Jeff Chen <jeffrey@cmu.edu>
2014-02-21 11:44:37 -0500

    background.js properly detects when the gmusic tab disappears

--- scripts/background.js
+++ scripts/background.js
@@ -4,10 +4,23 @@ chrome.storage.local.set({'id': '-1'});
 
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
   chrome.storage.local.get('id', function (data) {
-    //check if tab has been closed
-    if (changeInfo.url && changeInfo.url.search('play.google.com/music') != -1 && 
+    if (changeInfo.url &&
+        changeInfo.url.search('play.google.com/music') != -1 && 
         data['id'] == '-1') {
       chrome.storage.local.set({'id': tabId});
     }
+    else if (changeInfo.url &&
+             changeInfo.url.search('play.google.com/music') == -1 &&
+             data['id'] == tabId) {
+      chrome.storage.local.set({'id': '-1'});
+    }
   });
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId, removeInfo) {
+  chrome.storage.local.get('id', function (data) {
+    if (data['id'] == tabId) {
+      chrome.storage.local.set({'id': '-1'});
+    }
+  })
 });

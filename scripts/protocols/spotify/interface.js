39623d468b9e016899c9f816402e17b77ac36c31
Jeff Chen <jeffrey@cmu.edu>
2015-03-02 17:10:51 -0500

    Begin work on spotify integration, album art and sliders need work

---
+++ scripts/protocols/spotify/interface.js
@@ -0,0 +1,125 @@
+// Interfaces with the spotify tab
+
+$(function() {
+  var background_port = chrome.runtime.connect({name: "interface"});
+  var popup_port = null;
+  var old_status = null;
+
+  function update(slider, vslider) {
+    old_status = JSON.parse(JSON.stringify(music_status));
+    music_status.update();
+    music_status.slider_updated = (slider == true);
+    music_status.vslider_updated = (vslider == true);
+    // socket.emit('data', music_status);
+    var msg = create_background_msg(old_status, music_status);
+    if (msg != null) {
+      background_port.postMessage(msg);
+    }
+    if (popup_port) {
+      popup_port.postMessage(music_status);
+    }
+  }
+
+  function create_background_msg(oldValue, newValue) {
+    var msg = {scrobble: false, notify: false};
+    msg.oldValue = oldValue;
+    msg.newValue = newValue;
+    if (oldValue !== undefined && (oldValue.title != newValue.title ||
+        oldValue.artist != newValue.artist || oldValue.album_art != newValue.album_art)) {
+      msg.scrobble = true;
+      if (newValue.title != '') {
+        msg.notify = true;
+      }
+      return msg;
+    }
+    else {
+      return null;
+    }
+  }
+
+  slider = document.getElementById('app-player').contentWindow.document.getElementById('bar-click');
+  $(slider).click(function(evt) {console.log(evt);});
+
+  //TODO: fix
+  function update_slider(position, slider) {  //position is in %
+    var slider;
+    if (slider == 'slider') {
+      slider = document.getElementById('app-player').contentWindow.document.getElementById('bar-click');
+    }
+    else if (slider == 'vslider') {
+      $('#app-player').contents().find('#volume').mouseenter();
+      slider = document.getElementById('app-player').contentWindow.document.getElementById('volume-click');
+    }
+    var newWidth = Math.round(position * slider.offsetWidth);
+    var rect = slider.getBoundingClientRect();
+
+    slider.dispatchEvent(new MouseEvent('click', {
+      clientX: newWidth + rect.left + slider.clientLeft - slider.scrollLeft,
+      clientY: rect.top + slider.clientTop - slider.scrollTop,
+      bubbles: true
+    }));
+  }
+
+  function send_command(message) {
+    var iframe = $('#app-player').contents();
+    var $button = null;
+    switch (message.type) {
+      case 'play':
+        $button = iframe.find('#play-pause'); break;
+      case 'rew':
+        $button = iframe.find('#previous'); break;
+      case 'ff':
+        $button = iframe.find('#next'); break;
+      case 'shuffle':
+        $button = iframe.find('#shuffle'); break;
+      case 'repeat':
+        $button = iframe.find('#repeat'); break;
+      case 'slider':
+        update_slider(message.position, 'slider'); break;
+      case 'vslider':
+        update_slider(message.position, 'vslider'); break;
+    }
+    if ($button !== null) {
+      $button.click();
+    }
+    window.setTimeout( function() {
+      update(message.type == 'slider', message.type == 'vslider');
+    }, 30);
+  }
+
+  function parseMessage(msg) {
+    if (msg.action === 'update_status') {
+      update();
+    }
+    if (msg.action === 'send_command') {
+      send_command(msg);
+    }
+  }
+
+  // var socket = io('https://miniplay.herokuapp.com');
+  // socket.on('connect', function() {
+  //   // TODO: find a better selector (user and authuser might not be 0)
+  //   var email = $('a[href="/music/listen?u=0&authuser=0"] > div:contains("(default)") > div:contains("(default)")').text().split(' ')[0];
+  //   socket.emit('room', {client : 'player', room : email});
+  // });
+  // socket.on('data', parseMessage);
+
+  background_port.onMessage.addListener(parseMessage);
+
+  chrome.runtime.onConnect.addListener(function(port) {
+    if (port.name == 'popup') {
+      popup_port = port;
+      port.onDisconnect.addListener(function() {
+        popup_port = null;
+      });
+      port.onMessage.addListener(function(msg) {
+        if (msg.action === 'send_command') {
+          send_command(msg);
+        }
+      });
+      update();
+    }
+  });
+
+  window.setInterval(update, 1000);
+});
---
+++ scripts/protocols/spotify/status.js
@@ -0,0 +1,50 @@
+var music_status = {
+
+  disabled_buttons : ['up', 'down'],
+  artist : '',
+  album : '',
+  album_art : '',
+  title : '',
+  current_time : '',
+  total_time : '',
+  current_time_s : 0,
+  total_time_s: 0,
+  thumb : '0',
+  repeat : 'NO_REPEAT',
+  shuffle : 'NO_SHUFFLE',
+  status : '',
+  volume : '',
+  slider_updated : false,
+  vslider_updated : false,
+  protocol : 'spotify',
+
+  get_time : function (time) {
+    return time.split(':').map(function(num, index, arr) {
+      return Math.abs(parseInt(num, 10) * Math.pow(60, arr.length - index - 1));
+    }).reduce(function(a, b) { return a + b; });
+  },
+
+  get_album_art : function () {
+    var url = $('#app-player').contents().find('.sp-image-img').css('background-image');
+    return url ? url.substring(url.search('http'), url.search(';') - 1) : 'img/default_album.png';
+  },
+
+  update : function() {
+    var iframe = $('#app-player').contents();
+    this.title = iframe.find('#track-name > a').text();
+    this.artist = iframe.find('#track-artist > a').text();
+    // this.album = $('.playerBarAlbum').text();
+    this.album_art = this.get_album_art();
+    this.current_time = iframe.find('#track-current').text();
+    this.total_time = iframe.find('#track-length').text();
+    this.current_time_s = this.get_time(this.current_time);
+    this.total_time_s = this.get_time(this.total_time);
+    this.repeat = iframe.find('#repeat').hasClass('active') ? 'LIST_REPEAT' : 'NO_REPEAT';
+    this.shuffle = iframe.find('#shuffle').hasClass('active') ? 'ALL_SHUFFLE' : 'NO_SHUFFLE';
+    this.status = (iframe.find('#play-pause').hasClass('playing')) ? 'Pause' : 'Play';
+    this.volume = 100;
+    this.slider_updated = false;
+    this.vslider_updated = false;
+    return this;
+  }
+};

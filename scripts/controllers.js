473ba36713bfe1c69458b6630fb293f6e3144aad
Jeff Chen <jeffrey@cmu.edu>
2015-07-20 14:03:49 -0400

    Change playlist scroll behavior and add equalizer animation to playing song

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -148,7 +148,10 @@ var controller = popupApp.controller('PopupController', ['$scope', function($sco
               $scope.set_disabled(response.disabled_buttons);
 
               for (var i = 0; i < response.playlist.length; i++) {
-                if (response.playlist[i].title && ($scope.playlist.length <= i || response.playlist[i].title != $scope.playlist[i].title)) {
+                if (response.playlist[i].title &&
+                    ($scope.playlist.length <= i ||
+                     response.playlist[i].title != $scope.playlist[i].title ||
+                     response.playlist[i].currently_playing != $scope.playlist[i].currently_playing)) {
                   $scope.playlist[i] = response.playlist[i];
                   $scope.playlist[i].index = i;
                 }
--- scripts/directives.js
+++ scripts/directives.js
@@ -92,17 +92,8 @@ popupApp.directive('mpScrollIf', function() {
       }, function(value) {
         if (value) {
           var sp = getScrollingParent(element[0]);
-          var topMargin = parseInt(attrs.scrollMarginTop) || 0;
-          var bottomMargin = parseInt(attrs.scrollMarginBottom) || 0;
-          var elemOffset = element[0].offsetTop;
-          var elemHeight = element[0].clientHeight;
-
           if (sp) {
-            if (elemOffset - topMargin < sp.scrollTop) {
-                sp.scrollTop = elemOffset - topMargin;
-            } else if (elemOffset + elemHeight + bottomMargin > sp.scrollTop + sp.clientHeight) {
-                sp.scrollTop = elemOffset + elemHeight + bottomMargin - sp.clientHeight;
-            }
+            sp.scrollTop = $(element[0]).offset().top;
           }
         }
       });

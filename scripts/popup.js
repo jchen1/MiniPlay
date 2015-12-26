17b0eb03f9728863a8175d22d07212b9512b3f94
Jeff Chen <jeffrey@cmu.edu>
2015-12-26 11:44:35 -0800

    Allow images from chrome-extension://

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,1 +1,5 @@
 var popupApp = angular.module('app', []);
+
+popupApp.config(['$compileProvider', function ($compileProvider) {
+    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|local|data|chrome-extension):/);
+}]);
